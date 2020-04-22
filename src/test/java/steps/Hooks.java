c2944b890c64ac8073f87d58e04c949b52a718e1
mohammed-a-ali <m.3li.abdallah@gmail.com>
2020-04-22 02:53:33 +0200

    Add images

--- src/test/java/steps/Hooks.java
+++ src/test/java/steps/Hooks.java
@@ -4,13 +4,5 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 
 
 public class Hooks extends AbstractTestNGCucumberTests {
-//    @Before
-//    public void openBrowser(){
-//        Driver.startDriver();
-//    }
-//
-//    @After
-//    public void closeBrowser(){
-//        Driver.stopDriver();
-//    }
+
 }
